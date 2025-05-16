0aaef9f50728cf574e6519354d96d9b6ca807d02
msouuza@live.com <msouuza@live.com>
2025-05-16 16:25:39 -0300

    love

--- script.js
+++ script.js
@@ -12,6 +12,6 @@
     });
 
     document.getElementById('sim').addEventListener('click', () => {
-      window.location.href = 'https://www.instagram.com/p/DITP6SNs_5q/';
+      window.location.href = 'https://www.youtube.com/watch?v=6h4BuzcCDfI';
     });
     
