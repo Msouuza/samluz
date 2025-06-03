95721a18c6575d5da18966e437a521846c8be68a
msouuza@live.com <msouuza@live.com>
2025-06-03 13:48:21 -0300

    love

--- script.js
+++ script.js
@@ -12,6 +12,6 @@
     });
 
     document.getElementById('sim').addEventListener('click', () => {
-      window.location.href = 'https://www.youtube.com/watch?v=6h4BuzcCDfI';
+      window.location.href = 'https://www.youtube.com/watch?v=51lOFuCR7dE';
     });
     
